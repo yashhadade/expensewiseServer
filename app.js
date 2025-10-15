5e3ea556760b2e9bca88d81421297ab065b45cf8
YashhRealx <yash.hadade@realx.in>
2025-10-15 19:38:03 +0530

    add the vm deploayment

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/request", requestAPI)
 //     res.redirect('https://expensewisee.vercel.app/');
 // })
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('HIIII the backend is Runnibg !');
 });
 
 
